7116d797b30fc7ea1b47a03da8d330bd84b766fd
unknown <preetiverma16lko@gmail.com>
2019-05-20 15:55:07 +0530

    changes

--- header.php
+++ header.php
@@ -24,7 +24,7 @@
 			<a href="mailto:connect@biharfraternity.org"><i class="fa fa-envelope" style="margin-right: 4px;color:#fff;font-size: 13px;"></i>connect@biharfraternity.org</a>
 		</div> -->
 		<nav class="navbar navbar-expand-md bg-defualt navbarListingtop">
-		  <a class="navbar-brand" href="index.html"><img src="images/logo.png" style="width: 100px;"></a>
+		  <a class="navbar-brand" href="index.php"><img src="images/logo.png" style="width: 100px;"></a>
 		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
 		    <span class="navbar-toggler-icon navBarr">
 		    	<i class="fa fa-bars"></i>
@@ -33,36 +33,36 @@
 		  <div class="collapse navbar-collapse navbarright" id="collapsibleNavbar">
 		    <ul class="navbar-nav">
 		      <li class="nav-item mobile-dropdown">
-		        <a class="nav-link navListing" href="about.html">About
+		        <a class="nav-link navListing" href="about.php">About
 		        <i class="fa fa-chevron-down"></i></a>
 		        <ul class="dropdown mobileToggle">
-		        	<li><a href="our-team.html">Our Team</a></li>
+		        	<li><a href="our-team.php">Our Team</a></li>
 		        </ul>
 		      </li>
 		      <li class="nav-item">
-		        <a class="nav-link navListing" href="project.html">Projects</a>
+		        <a class="nav-link navListing" href="project.php">Projects</a>
 		      </li>
 		      <li class="nav-item">
-		        <a class="nav-link navListing" href="event.html">Events</a>
+		        <a class="nav-link navListing" href="event.php">Events</a>
 		      </li>
 		       <li class="nav-item">
-		        <a class="nav-link navListing" href="gallery.html">Gallery</a>
+		        <a class="nav-link navListing" href="gallery.php">Gallery</a>
 		      </li>
 		       <li class="nav-item mobile-dropdown1">
 		        <a class="nav-link navListing" href="javascript:void(0);">Know Bihar<i class="fa fa-chevron-down"></i></a>
 		        <ul class="dropdown mobileToggle1">
-		        	<li><a href="place.html">Places</a></li>
-		        	<li><a href="food.html">Food</a></li>
-		        	<li><a href="festival.html">Festivals</a></li>
-		        	<li><a href="art.html">Art</a></li>
-		        	<li><a href="people.html">People</a></li>
+		        	<li><a href="place.php">Places</a></li>
+		        	<li><a href="food.php">Food</a></li>
+		        	<li><a href="festival.php">Festivals</a></li>
+		        	<li><a href="art.php">Art</a></li>
+		        	<li><a href="people.php">People</a></li>
 		        </ul>
 		      </li>
 		      <li class="nav-item">
-		        <a class="nav-link navListing" href="testimonial.html">Testimonials</a>
+		        <a class="nav-link navListing" href="testimonial.php">Testimonials</a>
 		      </li>
 		      <li class="nav-item">
-		        <a class="nav-link navListing" href="contact.html">Contact us</a>
+		        <a class="nav-link navListing" href="contact.php">Contact us</a>
 		      </li>    
 		    </ul>
 		  </div>  
@@ -71,4 +71,3 @@
 		</div>
 	</header>
 
-	
