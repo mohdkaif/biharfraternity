4510c72b71b2b4a778d994daf6880ccdd704279e
unknown <preetiverma16lko@gmail.com>
2019-05-28 16:23:48 +0530

    changes

--- event.php
+++ event.php
@@ -1,16 +1,16 @@
 <?php
 require('header.php');
 ?>
-<section class="team-section our_team_page">
+<section class="team-section our_team_page backgroundgray">
 		<div class="container">
 			<div class="row text-center">
     			<div class="col-md-12">
-    				<h2 class="tittlehead tittlehead-inner">Our Events</h2>
+    				<h2 class="tittlehead tittlehead-inner">Upcoming Event</h2>
     			</div>
 		    </div>
-		    <div class="row">
+		    <div class="row text-center">
     			<div class="col-md-12">
-    				
+    				<div class="event-img"><img src="images/upcoming-event.jpg" alt="event"></div>
     			</div>
 		    </div>
 		</div>
