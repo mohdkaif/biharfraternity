e25c1e136a370a34388c1f8f367a5e4799714b36
unknown <preetiverma16lko@gmail.com>
2019-05-29 13:21:49 +0530

    changes

--- gallery.php
+++ gallery.php
@@ -122,7 +122,7 @@ require('header.php');
 						<div class="col-md-7 col-sm-12">
 							<div class="gallerycontent">
 								<h2 class="eventname">Project Jyoti</h2>
-								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+								<p></p>
 								
 							</div>
 						</div>
